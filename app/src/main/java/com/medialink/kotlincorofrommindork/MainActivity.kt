faa16ff8bf4364a10063393f8f5da34f5b392566
Benk Home <mail.buat.forum@gmail.com>
2020-06-09 00:15:57 +0700

    init

---
+++ app/src/main/java/com/medialink/kotlincorofrommindork/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.medialink.kotlincorofrommindork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+// sumber: https://blog.mindorks.com/using-retrofit-with-kotlin-coroutines-in-android
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
